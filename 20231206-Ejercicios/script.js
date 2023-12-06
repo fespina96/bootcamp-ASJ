b958b063476e402dfc7552fd412a848bb960299e
Fer Espina <fespina96@gmail.com>
2023-12-06 11:37:30 -0300

    06122023

--- 20231206-Ejercicios/script.js
+++ 20231206-Ejercicios/script.js
@@ -1,6 +1,6 @@
 let user_input = document.getElementById("user-input");
 let output_p = document.getElementById("output-p");
-const vocales = ["a","e","i","o","u"]
+const vocales = "aAeEiIoOuU";
 
 const sacarVocales = () => {
     let string_input = user_input.value;
@@ -18,8 +18,4 @@ const sacarVocales = () => {
     output_p.innerText = string_output;
 }
 
-user_input.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      sacarVocales();
-    }
-});
+user_input.addEventListener('input',()=> sacarVocales());
