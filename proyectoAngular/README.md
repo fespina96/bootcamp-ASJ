becde1ce8f8e4b4a0c7548e8c8e839f681f35b56
Fer Espina <fespina96@gmail.com>
2023-12-12 15:40:54 -0300

    12122023

--- proyectoAngular/README.md
+++ proyectoAngular/README.md
@@ -1,4 +1,4 @@
-# AngularApp
+# ProyectoAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.6.
 
--- proyectoAngular/src/app/app.component.spec.ts
+++ proyectoAngular/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'AngularApp' title`, () => {
+  it(`should have as title 'proyectoAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularApp');
+    expect(app.title).toEqual('proyectoAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularApp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proyectoAngular');
   });
 });
